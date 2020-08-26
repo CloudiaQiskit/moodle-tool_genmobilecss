a15bb4c0f381e1bbda2b8d0f28dcbb5d371bb6b4
Alison Bell <sheesania@users.noreply.github.com>
2020-08-26 12:13:25 -0600

    no concatenation in lang files

--- lang/en/tool_genmobilecss.php
+++ lang/en/tool_genmobilecss.php
@@ -23,28 +23,21 @@
  */
 
 $string['pluginname'] = 'Generate custom CSS for Moodle Mobile';
-$string['introtext'] = "This is a tool for generating custom CSS for the Moodle Mobile app, especially overriding " .
-        "the default colors. It will guide you through three simple steps:";
+$string['introtext'] = "This is a tool for generating custom CSS for the Moodle Mobile app, especially overriding the default colors. It will guide you through three simple steps:";
 $string['introstep1'] = "1. Download the latest Moodle Mobile default CSS.";
-$string['introstep2'] = "2. View the latest default CSS's color scheme and pick out any alternate colors " .
-        "you prefer, plus add any extra custom CSS.";
-$string['introstep3'] = "3. Generate a custom CSS file to override the default colors and add your extra custom CSS, " .
-        "and set it as your Moodle Mobile CSS.";
+$string['introstep2'] = "2. View the latest default CSS's color scheme and pick out any alternate colors you prefer, plus add any extra custom CSS.";
+$string['introstep3'] = "3. Generate a custom CSS file to override the default colors and add your extra custom CSS, and set it as your Moodle Mobile CSS.";
 $string['introcalltoaction'] = "Ready to get started? This download may take a moment!";
 $string['downloadmobilecss'] = 'Download latest mobile CSS';
 $string['cachedef_mobilecss'] = 'The current default CSS file for the Moodle Mobile app';
 $string['cachedef_colors'] = 'Information about colors found in the default CSS file';
 $string['uses'] = 'uses';
-$string['colorformdesc'] = 'Choose alternate colors for any default colors you would like to replace. If you don\'t ' .
-        'choose an alternate color, the default one will continue to be used. You can also add any other custom ' .
-        'mobile CSS.';
-$string['colorformcalltoaction'] = 'When you\'re done, click the one of the "Generate custom CSS" buttons at the top ' .
-        'and bottom of the page! Generating your CSS may take a moment.';
+$string['colorformdesc'] = 'Choose alternate colors for any default colors you would like to replace. If you don\'t choose an alternate color, the default one will continue to be used. You can also add any other custom mobile CSS.';
+$string['colorformcalltoaction'] = 'When you\'re done, click the one of the "Generate custom CSS" buttons at the top and bottom of the page! Generating your CSS may take a moment.';
 $string['customcsslabel'] = 'Additional custom CSS';
 $string['replacementcolor'] = 'Color to replace ';
 $string['willbeconvertedto'] = 'will be converted to';
 $string['colorformsubmit'] = 'Generate custom CSS file with these colors';
 $string['urldesc'] = 'URL for your custom mobile CSS';
-$string['urlinstructions'] = 'Copy this URL, click the button below to go to the mobile appearance settings, then paste the URL '.
-        'in the CSS setting and save your changes.';
+$string['urlinstructions'] = 'Copy this URL, click the button below to go to the mobile appearance settings, then paste the URL in the CSS setting and save your changes.';
 $string['gotosettings'] = 'Go to mobile appearance settings';
